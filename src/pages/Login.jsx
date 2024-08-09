604d958090a52e117375538d922cb7125cfe6776
Harshit P G <harshitpg2004@gmail.com>
2024-08-09 20:01:38 +0530

    deploy ready ?

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,34 @@
-
 import { GrGoogle } from "react-icons/gr";
-import { GOOGLE_AUTH_REDIRECT } from "../config";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
   const googleLogin = () => {
-    window.location.href = GOOGLE_AUTH_REDIRECT;
+    window.location.href = import.meta.env.VITE_GOOGLE_AUTH_REDIRECT;
   };
-  useEffect(()=>{
+  useEffect(() => {
     const token = localStorage.getItem("token");
-    if (token) navigate('/home');
-  }, [])
+    if (token) navigate("/home");
+  }, []);
   return (
     <main className="h-screen w-full flex justify-center items-center bg-black">
       <div className="flex flex-col items-center rounded-xl border-gray-900 w-[600px] p-6 px-10 gap-5 text-white bg-gray-800">
-        <div className="size-14"><img src="/admin-portal-icon-3.png" alt="admin-portal-icon" /></div>
+        <div className="size-14">
+          <img src="/admin-portal-icon-3.png" alt="admin-portal-icon" />
+        </div>
         <div className="text-4xl font-bold">Sign in to Admin Portal</div>
-        <div className="text-xl text-center">For a seamless and secure experience, please sign in using your Google account.</div>
-        <button className="border border-gray-500 hover:bg-gray-500 rounded-md px-5 py-2 w-[75%] flex items-center justify-center gap-3" onClick={googleLogin}>
-          <span><GrGoogle className="size-4" /></span>
+        <div className="text-xl text-center">
+          For a seamless and secure experience, please sign in using your Google
+          account.
+        </div>
+        <button
+          className="border border-gray-500 hover:bg-gray-500 rounded-md px-5 py-2 w-[75%] flex items-center justify-center gap-3"
+          onClick={googleLogin}
+        >
+          <span>
+            <GrGoogle className="size-4" />
+          </span>
           Sign in with Google
         </button>
       </div>
